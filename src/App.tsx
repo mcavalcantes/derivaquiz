b9653ecbd93dfe98a8dd608b5e7f09b7fc6d8c2e
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2025-02-24 08:21:44 -0300

    built the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Github } from "./icons/iconmonstr/Github";
+
 export function App() {
   return (
-    <div className="font-bold text-6xl">hello world</div>
+    <div className="min-h-screen flex flex-col">
+      <header></header>
+      <main className="debug px-8">
+        {/* display */}
+        {/* div c/ 3 answers */}
+      </main>
+      <footer className="h-12 mt-auto select-none flex items-center justify-center gap-2">
+        <a
+          href="mailto:matheuscavalcantes.mc@gmail.com"
+          className="font-semibold text-sm underline underline-offset-2"
+        >
+          Contato
+        </a>
+        <p>|</p>
+        <a
+          href="https://github.com/mcavalcantes/derivaquiz"
+          className="flex items-center gap-2 font-semibold text-sm underline underline-offset-2"
+        >
+          <Github />
+          <p>Repositório</p>
+        </a>
+      </footer>
+    </div>
   );
 }
